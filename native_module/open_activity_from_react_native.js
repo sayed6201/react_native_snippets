81af79e75d3c98588ad2e00595500864d7a7545b
sayed6201 <sayed.ahmed620@gmail.com>
2023-04-25 19:38:50 +0100

    open activity from js

---
+++ native_module/open_activity_from_react_native.js
@@ -0,0 +1,9 @@
+
+//create Activity class with XML UI
+//Than call this from
+@ReactMethod
+void navigateToExample() {
+    ReactApplicationContext context = getReactApplicationContext();
+    Intent intent = new Intent(context, NativeUI.class);
+    context.startActivity(intent);
+}
